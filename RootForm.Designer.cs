df3e41d54c2ce4d3ee64332a100813ecbbc4047d
ADMIN <honganh.book@gmail.com>
2024-03-25 13:17:06 +0700

    Add project files.

---
+++ RootForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MinigameOlympia
+{
+    partial class RootForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.btnQuenMK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(324, 110);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(141, 53);
+            this.btnDangNhap.TabIndex = 0;
+            this.btnDangNhap.Text = "Dang Nhap";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Location = new System.Drawing.Point(330, 199);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(141, 53);
+            this.btnDangKy.TabIndex = 1;
+            this.btnDangKy.Text = "Dang Ky";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            // 
+            // btnQuenMK
+            // 
+            this.btnQuenMK.Location = new System.Drawing.Point(330, 284);
+            this.btnQuenMK.Name = "btnQuenMK";
+            this.btnQuenMK.Size = new System.Drawing.Size(141, 53);
+            this.btnQuenMK.TabIndex = 2;
+            this.btnQuenMK.Text = "QuenMK";
+            this.btnQuenMK.UseVisualStyleBackColor = true;
+            // 
+            // RootForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnQuenMK);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.btnDangNhap);
+            this.Name = "RootForm";
+            this.Text = "RootForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.Button btnQuenMK;
+    }
+}
+
