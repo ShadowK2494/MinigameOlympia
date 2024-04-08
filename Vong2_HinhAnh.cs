43f4efe7d47b772f3a09dd866954ce21c5f9f624
Thai Trinh <thaitrinh12100@gmail.com>
2024-04-08 22:28:45 +0700

    Vong 2 giao dien

---
+++ Vong2_HinhAnh.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinigameOlympia
+{
+    public partial class Vong2_HinhAnh : Form
+    {
+        public Vong2_HinhAnh()
+        {
+            InitializeComponent();
+        }
+    }
+}
