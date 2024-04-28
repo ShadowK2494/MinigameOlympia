faf91f298e10e2b90fd23bd17aa5a17d53e0796b
Anh Anh <honganh.book@gmail.com>
2024-04-28 19:06:12 +0700

    Models vs RatingController vs RoomController

---
+++ Models/GroupQuestion.cs
@@ -0,0 +1,10 @@
+﻿namespace OlympiaWebService.Models
+{
+    public class GroupQuestion
+    {
+        public string IDQuestion { get; set; }
+        public string IDGroup { get; set; }
+        public Question Main { get; set; }
+        public Question Member { get; set; }
+    }
+}
