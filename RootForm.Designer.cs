e1198b2840961124adf7ab570976929e1ff9f811
Anh Anh <honganh.book@gmail.com>
2024-05-20 11:15:52 +0700

    RootForm vs

--- RootForm.Designer.cs
+++ RootForm.Designer.cs
@@ -34,34 +34,39 @@
             // 
             // btnDangNhap
             // 
-            this.btnDangNhap.Location = new System.Drawing.Point(216, 72);
-            this.btnDangNhap.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDangNhap.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDangNhap.Location = new System.Drawing.Point(82, 488);
             this.btnDangNhap.Name = "btnDangNhap";
-            this.btnDangNhap.Size = new System.Drawing.Size(94, 34);
+            this.btnDangNhap.Size = new System.Drawing.Size(168, 68);
             this.btnDangNhap.TabIndex = 0;
-            this.btnDangNhap.Text = "Dang Nhap";
-            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Text = "ĐĂNG NHẬP";
+            this.btnDangNhap.UseVisualStyleBackColor = false;
             this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
             // 
             // btnDangKy
             // 
-            this.btnDangKy.Location = new System.Drawing.Point(220, 129);
-            this.btnDangKy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDangKy.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnDangKy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangKy.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDangKy.Location = new System.Drawing.Point(265, 488);
             this.btnDangKy.Name = "btnDangKy";
-            this.btnDangKy.Size = new System.Drawing.Size(94, 34);
+            this.btnDangKy.Size = new System.Drawing.Size(168, 68);
             this.btnDangKy.TabIndex = 1;
-            this.btnDangKy.Text = "Dang Ky";
-            this.btnDangKy.UseVisualStyleBackColor = true;
+            this.btnDangKy.Text = "ĐĂNG KÝ";
+            this.btnDangKy.UseVisualStyleBackColor = false;
             this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
             // 
             // RootForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 224);
+            this.BackgroundImage = global::MinigameOlympia.Properties.Resources.BackgroundLogin;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(497, 631);
             this.Controls.Add(this.btnDangKy);
             this.Controls.Add(this.btnDangNhap);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "RootForm";
             this.Text = "RootForm";
             this.ResumeLayout(false);
