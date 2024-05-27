9d2d206f69920b8f80a84c9856f408a5c4502b97
PhungVietBac <22520089@gm.uit.edu.vn>
2024-05-27 12:05:11 +0700

    Fix QuenMK

--- QuenMK.Designer.cs
+++ QuenMK.Designer.cs
@@ -32,12 +32,6 @@
             this.label5 = new System.Windows.Forms.Label();
             this.tbOTP = new System.Windows.Forms.TextBox();
             this.btnSubmit = new System.Windows.Forms.Button();
-            this.textBoxEmail = new System.Windows.Forms.TextBox();
-            this.buttonSend = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBoxCode = new System.Windows.Forms.TextBox();
-            this.buttonVerify = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label6 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lblAlertEmail = new System.Windows.Forms.Label();
@@ -100,25 +94,6 @@
             this.btnSendOTP.Text = "Gửi";
             this.btnSendOTP.UseVisualStyleBackColor = true;
             this.btnSendOTP.Click += new System.EventHandler(this.btnSendOTP_Click);
-            // textBoxEmail
-            // 
-            this.textBoxEmail.Location = new System.Drawing.Point(387, 87);
-            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBoxEmail.Multiline = true;
-            this.textBoxEmail.Name = "textBoxEmail";
-            this.textBoxEmail.Size = new System.Drawing.Size(408, 46);
-            this.textBoxEmail.TabIndex = 4;
-            // 
-            // buttonSend
-            // 
-            this.buttonSend.Location = new System.Drawing.Point(812, 87);
-            this.buttonSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.buttonSend.Name = "buttonSend";
-            this.buttonSend.Size = new System.Drawing.Size(85, 46);
-            this.buttonSend.TabIndex = 5;
-            this.buttonSend.Text = "Gửi";
-            this.buttonSend.UseVisualStyleBackColor = true;
-            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
             // 
             // label5
             // 
@@ -153,37 +128,6 @@
             this.btnSubmit.UseVisualStyleBackColor = true;
             this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
-            // textBoxCode
-            // 
-            this.textBoxCode.Location = new System.Drawing.Point(387, 190);
-            this.textBoxCode.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBoxCode.Multiline = true;
-            this.textBoxCode.Name = "textBoxCode";
-            this.textBoxCode.Size = new System.Drawing.Size(408, 47);
-            this.textBoxCode.TabIndex = 7;
-            // 
-            // buttonVerify
-            // 
-            this.buttonVerify.Location = new System.Drawing.Point(812, 190);
-            this.buttonVerify.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.buttonVerify.Name = "buttonVerify";
-            this.buttonVerify.Size = new System.Drawing.Size(83, 47);
-            this.buttonVerify.TabIndex = 8;
-            this.buttonVerify.Text = "Xác nhận";
-            this.buttonVerify.UseVisualStyleBackColor = true;
-            this.buttonVerify.Click += new System.EventHandler(this.buttonVerify_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::MinigameOlympia.Properties.Resources.BackgroundLogin;
-            this.pictureBox1.Location = new System.Drawing.Point(-3, -1);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(356, 453);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 19;
-            this.pictureBox1.TabStop = false;
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
@@ -207,7 +151,6 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 19;
             this.pictureBox1.TabStop = false;
-            this.label6.Text = "Back";
             // 
             // lblAlertEmail
             // 
@@ -220,14 +163,6 @@
             this.lblAlertEmail.TabIndex = 30;
             this.lblAlertEmail.Text = "a";
             this.lblAlertEmail.Visible = false;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label1.Location = new System.Drawing.Point(925, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(22, 23);
-            this.label1.TabIndex = 21;
-            this.label1.Text = "X";
             // 
             // ForgetPassword
             // 
@@ -242,11 +177,6 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.btnSendOTP);
             this.Controls.Add(this.tbEmail);
-            this.Controls.Add(this.buttonVerify);
-            this.Controls.Add(this.textBoxCode);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.buttonSend);
-            this.Controls.Add(this.textBoxEmail);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -271,11 +201,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox tbOTP;
         private System.Windows.Forms.Button btnSubmit;
-        private System.Windows.Forms.TextBox textBoxEmail;
-        private System.Windows.Forms.Button buttonSend;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBoxCode;
-        private System.Windows.Forms.Button buttonVerify;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label lblAlertEmail;
